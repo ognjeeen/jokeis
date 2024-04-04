cd6ddf41a5a8a044b3eaa1dfd6d39616267d3288
Ognjen <contact.ognjen@gmail.com>
2024-04-04 16:42:49 +0200

    Database Connection

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,11 @@ import Link from 'next/link';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Welcome</h1>
-      <Link href="/jokes">Show Jokes</Link>
+    <div className="flex items-center justify-center">
+      <div>
+        <h1 className="text-3xl">Welcome</h1>
+        <Link href="/jokes">Show Jokes</Link>
+      </div>
     </div>
   );
 };
