29b831e3b07db3de73aaadc7c94ad8653d380c6c
Ognjen <contact.ognjen@gmail.com>
2024-04-22 14:19:42 +0200

    Middleware update

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/jokes/add', '/profile', '/jokes/saved'],
+  matcher: ['/jokes/add', '/profile', '/jokes/saved', '/jokes/liked'],
 };
